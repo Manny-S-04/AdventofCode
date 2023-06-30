f1de7f8ce6f001c84e8273893580f3bc1b7bbe6d
manny <mansimran2001@googlemail.com>
2023-06-30 11:59:37 +0100

    added day2 txt

--- webscrapper.py
+++ webscrapper.py
@@ -1,17 +1,20 @@
 import requests
-import mechanize
 from bs4 import BeautifulSoup
 import numpy as np
 from credentials import User
+from os.path import exists
+import time
 
-def login(day:int):
+
+def login(day: int):
     """ 
     
         Method to retrieve Advent of Code data
     
-        Login method will take in the day number which is an integer which will be concatenated to the url to retrieve the associated data
+        Login method will take in the day number which is an integer which will be concatenated to the url to
+        retrieve the associated data
         
-        The method works by making a session and getting the login url by github
+        The method works by making a session and getting the login url by GitHub
         
         It then parses the html and searches for the forms where it will send a username and password 
         
@@ -22,11 +25,14 @@ def login(day:int):
     
     """
     session = requests.Session()
-    
-    response = session.get("https://github.com/login?client_id=7bb0a7ec13388aa67963&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D7bb0a7ec13388aa67963%26duration%3Dtemporary%26redirect_uri%3Dhttps%253A%252F%252Fadventofcode.com%252Fauth%252Fgithub%252Fcallback%26response_type%3Dcode%26scope%3D%26state%3Dx")
+
+    response = session.get(
+        "https://github.com/login?client_id=7bb0a7ec13388aa67963&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id"
+        "%3D7bb0a7ec13388aa67963%26duration%3Dtemporary%26redirect_uri%3Dhttps%253A%252F%252Fadventofcode.com"
+        "%252Fauth%252Fgithub%252Fcallback%26response_type%3Dcode%26scope%3D%26state%3Dx")
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    
+
     form = soup.find('form')
 
     inputs = form.find_all('input')
@@ -40,7 +46,7 @@ def login(day:int):
     if response.status_code == 200:
 
         page_response = session.get(f"https://adventofcode.com/2022/day/{day}/input")
-        
+
         if page_response.status_code == 200:
 
             return page_response.text.splitlines()
@@ -51,11 +57,19 @@ def login(day:int):
 
 
 def savetotext(aocinput):
-    
-    inputToArray = np.array(aocinput, dtype="U")
-    
-    np.savetxt("day1.txt", inputToArray, fmt="%s", delimiter='\n\n')
-    
-    
-    
-savetotext(login(1))
+    inputtoarray = np.array(aocinput, dtype="U")
+
+    np.savetxt("day2.txt", inputtoarray, fmt="%s", delimiter='\n')
+
+    time.sleep(5)
+
+    if (exists("day2.txt")):
+        print("Done")
+    else:
+        print("File was not created")
+
+
+
+
+
+savetotext(login(2))
